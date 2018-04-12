af0bc340bd94f8f7dba50a140db0c2bde338528c
Landon Call <landon@thecallfamily.com>
2018-04-11 20:26:03 -0600

    separating footer out of templates and into it's own blade. As well as moving some files to the correct path.

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/generic', function () {
+    return view('generic');
+});
+
+Route::get('/elements', function () {
+    return view('elements');
 });
